4d69aeae8004faef608ec4b1e25c7f1b29d525f6
Noani <noanizingerbarbieri@hotmail.com>
2022-04-02 00:41:15 -0300

    Style: global style creation and assets import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { GlobalStyles } from './style';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <GlobalStyles>
+    <App />
+  </GlobalStyles>
+);
 
---
+++ src/style.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+import MarkPro from "./assets/fonts/MarkPro.ttf";
+import MarkProBold from "./assets/fonts/MarkProBold.ttf";
+
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    @font-face {
+        font-family: MarkPro;
+        src: url(${MarkPro});
+    }
+
+    @font-face {
+        font-family: MarkProBold;
+        src: url(${MarkProBold});
+    }
+`;
