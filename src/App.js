81c1c490ed8e2a640d076aecba51c5076d8bf50e
Noani <noanizingerbarbieri@hotmail.com>
2022-04-02 11:34:39 -0300

    Refactor: Restructuring of routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { LoginPage } from './Pages/LoginPage/index.jsx'
-import HomePage from './Pages/HomePage.jsx'
-import SearchPage from './Pages/SearchPage.jsx'
-import NotFound from './Pages/NotFoundPage.jsx'
+import PageRoutes from "./routes/routes";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route exact path='/' element={<LoginPage/>} />
-            <Route exact path='/home' element={<HomePage />} />
-            <Route exact path='/SearchPage' element={<SearchPage />}/>
-            <Route  path='*' element={<NotFound/>}/>
-        </Routes>
-    </BrowserRouter>
+    <PageRoutes/>
   );
 }
 
---
+++ src/routes/routes.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { LoginPage } from '../Pages/LoginPage/index.jsx'
+import HomePage from '../Pages/HomePage.jsx'
+import SearchPage from '../Pages/SearchPage.jsx'
+import NotFound from '../Pages/NotFoundPage.jsx'
+
+function PageRoutes() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route exact path='/' element={<LoginPage/>} />
+            <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/SearchPage' element={<SearchPage />}/>
+            <Route  path='*' element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default PageRoutes;
