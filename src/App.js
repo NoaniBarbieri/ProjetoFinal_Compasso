31fca070a8e76174c14c4e1361132cca75e73b2a
Noani <noanizingerbarbieri@hotmail.com>
2022-04-02 00:26:39 -0300

    Feat: creation of the routes and the base files of the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import LoginPage from './Pages/LoginPage'
+import HomePage from './Pages/HomePage'
+import SearchPage from './Pages/SearchPage'
+import NotFound from './Pages/NotFoundPage'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route exact path='/' element={<LoginPage/>} />
+            <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/SearchPage' element={<SearchPage />}/>
+            <Route exact path='*' element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
