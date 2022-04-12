36ecc4f8bc4ff36a7b2b509c2df29ac939addcb3
Noani <noanizingerbarbieri@hotmail.com>
2022-04-12 19:33:08 -0300

    Feat: validation creation with JSON

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContainerPage, ContainerLeft, ContainerRigth,ContainerTop,LoginText, FormContainer, ValidationContainer, ErrorMessage } from './style'
 import CompassLogoWhite from "../../assets/images/Logo-Compasso-Branco-hor 1.png"
 import { CompassLogoRigth, CompassLogoLeft } from '../../components/Images/style'
@@ -9,13 +9,30 @@ import LoginButton from '../../components/Buttons/ButtonLogin'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import{ schema } from '../../components/Validations/index'
+import { Check } from '../../components/SearchLogin/index'
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () =>  {
+  const navigate = useNavigate();
+  const [status, setStatus ] = useState({
+    menssage: ''
+  })
 
   const { handleSubmit, register, formState: { errors } } = useForm({
     mode: "onBlur",
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema,  { abortEarly: false })
   });
+
+  const onSubmit = (data) => {
+    const isUser = Check();
+    if(isUser){
+      navigate(('/home'));
+    }else{
+      setStatus({
+        menssage: 'Usuário Inválido'
+      })
+    }
+}
 
   return (
     <ContainerPage>
@@ -27,10 +44,11 @@ export const LoginPage = () =>  {
           </ContainerTop>
           <LoginText>Login</LoginText>
           <FormContainer>
-            <form onSubmit={handleSubmit(() => {console.log("entrei")})}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <LogUserInput  name="user" {...register("user")}/>
               <LogPassInput  name="password"  {...register("password")}/>
               <ValidationContainer>
+                <ErrorMessage>{status.menssage}</ErrorMessage>
                 <ErrorMessage >{errors.user?.message}</ErrorMessage>
                 <ErrorMessage >{errors.password?.message}</ErrorMessage>
               </ValidationContainer>
